2406b77d441323d57eb7166c7aff7a644b0ae191
VaemaVoid <78240180+Vaema@users.noreply.github.com>
2023-12-27 11:57:19 -0500

    Two material items.

---
+++ Content/Items/Materials/Auracris.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DVMod.Content.Items.Materials
+{
+    public class Auracris : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 25;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 56;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 14;
+            Item.height = 14;
+            Item.value = Item.sellPrice(0, 0, 0, 60);
+            Item.rare = ItemRarityID.Blue;
+            Item.useTime = 18;
+            Item.useAnimation = 18;
+            Item.useStyle = ItemUseStyleID.Swing;
+        }
+    }
+}
---
+++ Content/Items/Materials/DuskFlame.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DVMod.Content.Items.Materials
+{
+    public class DuskFlame : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 25;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 56;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.value = Item.sellPrice(0, 0, 0, 40);
+            Item.rare = ItemRarityID.Blue;
+            Item.useTime = 18;
+            Item.useAnimation = 18;
+            Item.useStyle = ItemUseStyleID.Swing;
+        }
+    }
+}
